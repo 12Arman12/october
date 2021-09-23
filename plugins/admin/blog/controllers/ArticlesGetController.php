ab9b7df5dbb732282876d85bb66142e624875b5c
12Arman12 <arman.saghatelyan.tw@gmail.com>
2021-09-23 12:03:08 +0400

    .

--- plugins/admin/blog/controllers/ArticlesGetController.php
+++ plugins/admin/blog/controllers/ArticlesGetController.php
@@ -4,23 +4,11 @@ use Backend\Classes\Controller;
 use BackendMenu;
 
 use Admin\Blog\Models\Article;
-
-
 use Illuminate\Http\JsonResponse;
 
 class ArticlesGetController extends Controller
 {
-    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ];
     
-    public $listConfig = 'config_list.yaml';
-    public $formConfig = 'config_form.yaml';
-    public $reorderConfig = 'config_reorder.yaml';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function get(): JsonResponse
     {
         $post = Article::with('category')->get();
